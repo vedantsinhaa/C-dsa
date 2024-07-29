a0569398ddb5096baad99646d68df1e9b8793e12
vedantsinhaa <130684311+vedantsinhaa@users.noreply.github.com>
2024-07-29 23:39:01 +0530

    Add files via upload

---
+++ Graph/BFS.cpp
@@ -0,0 +1,39 @@
+//O(V+E)--> TC
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e3 + 10;
+vector<int>graph[N];
+int vis[N];
+int level[N];
+
+void bfs(int source){
+    queue<int> q;
+    q.push(source);
+    vis[source]=1;
+        while(!q.empty()){
+            int curr_v = q.front();
+            q.pop();
+            cout << curr_v << " ";
+            for(int child : graph[curr_v]){
+                if(!vis[child]){
+                    q.push(child);
+                    vis[child]=1;
+                    level[child]=level[curr_v] + 1;
+                }
+            }
+        } 
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0;i<n-1;i++){
+        int v1, v2;
+        cin >> v1 >> v2;
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
+
+    }
+    bfs(1);
+}
